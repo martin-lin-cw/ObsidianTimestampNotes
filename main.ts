37b6dcf303ff7ffac1d7732b93227d32cea01f3c
martin-lin-cw <martin6061097@gmail.com>
2023-10-10 16:32:47 +0800

    fix local file streaming problem

--- main.ts
+++ main.ts
@@ -1,14 +1,21 @@
-import { Editor, MarkdownView, Plugin, Modal, App } from 'obsidian';
-import ReactPlayer from 'react-player/lazy'
+import { Editor, MarkdownView, Plugin, Modal, App, TFile } from "obsidian";
+import ReactPlayer from "react-player/lazy";
 
-import { VideoView, VIDEO_VIEW } from './view/VideoView';
-import { TimestampPluginSettings, TimestampPluginSettingTab, DEFAULT_SETTINGS } from 'settings';
+import { VideoView, VIDEO_VIEW } from "./view/VideoView";
+import {
+	TimestampPluginSettings,
+	TimestampPluginSettingTab,
+	DEFAULT_SETTINGS,
+} from "settings";
 
+const _videoNotFoundUrl = "https://www.youtube.com/watch?v=eBGIQ7ZuuiU";
 
 const ERRORS: { [key: string]: string } = {
-	"INVALID_URL": "\n> [!error] Invalid Video URL\n> The highlighted link is not a valid video url. Please try again with a valid link.\n",
-	"NO_ACTIVE_VIDEO": "\n> [!caution] Select Video\n> A video needs to be opened before using this hotkey.\n Highlight your video link and input your 'Open video player' hotkey to register a video.\n",
-}
+	INVALID_URL:
+		"\n> [!error] Invalid Video URL\n> The highlighted link is not a valid video url. Please try again with a valid link.\n",
+	NO_ACTIVE_VIDEO:
+		"\n> [!caution] Select Video\n> A video needs to be opened before using this hotkey.\n Highlight your video link and input your 'Open video player' hotkey to register a video.\n",
+};
 
 export default class TimestampPlugin extends Plugin {
 	settings: TimestampPluginSettings;
@@ -18,145 +25,193 @@ export default class TimestampPlugin extends Plugin {
 
 	async onload() {
 		// Register view
-		this.registerView(
-			VIDEO_VIEW,
-			(leaf) => new VideoView(leaf)
-		);
+		this.registerView(VIDEO_VIEW, (leaf) => new VideoView(leaf));
 
 		// Register settings
 		await this.loadSettings();
 
 		// Markdown processor that turns timestamps into buttons
-		this.registerMarkdownCodeBlockProcessor("timestamp", (source, el, ctx) => {
-			// Match mm:ss or hh:mm:ss timestamp format
-			const regExp = /\d+:\d+:\d+|\d+:\d+/g;
-			const rows = source.split("\n").filter((row) => row.length > 0);
-			rows.forEach((row) => {
-				const match = row.match(regExp);
-				if (match) {
-					//create button for each timestamp
-					const div = el.createEl("div");
-					const button = div.createEl("button");
-					button.innerText = match[0];
-					button.style.backgroundColor = this.settings.timestampColor;
-					button.style.color = this.settings.timestampTextColor;
+		this.registerMarkdownCodeBlockProcessor(
+			"timestamp",
+			(source, el, ctx) => {
+				// Match mm:ss or hh:mm:ss timestamp format
+				const regExp = /\d+:\d+:\d+|\d+:\d+/g;
+				const rows = source.split("\n").filter((row) => row.length > 0);
+				rows.forEach((row) => {
+					const match = row.match(regExp);
+					if (match) {
+						//create button for each timestamp
+						const div = el.createEl("div");
+						const button = div.createEl("button");
+						button.innerText = match[0];
+						button.style.backgroundColor =
+							this.settings.timestampColor;
+						button.style.color = this.settings.timestampTextColor;
 
-					// convert timestamp to seconds and seek to that position when clicked
-					button.addEventListener("click", () => {
-						const timeArr = match[0].split(":").map((v) => parseInt(v));
-						const [hh, mm, ss] = timeArr.length === 2 ? [0, ...timeArr] : timeArr;
-						const seconds = (hh || 0) * 3600 + (mm || 0) * 60 + (ss || 0);
-						if (this.player) this.player.seekTo(seconds);
-					});
-					div.appendChild(button);
-				}
-			})
-		});
-
+						// convert timestamp to seconds and seek to that position when clicked
+						button.addEventListener("click", () => {
+							const timeArr = match[0]
+								.split(":")
+								.map((v) => parseInt(v));
+							const [hh, mm, ss] =
+								timeArr.length === 2
+									? [0, ...timeArr]
+									: timeArr;
+							const seconds =
+								(hh || 0) * 3600 + (mm || 0) * 60 + (ss || 0);
+							if (this.player) this.player.seekTo(seconds);
+						});
+						div.appendChild(button);
+					}
+				});
+			}
+		);
 
 		// Markdown processor that turns video urls into buttons to open views of the video
-		this.registerMarkdownCodeBlockProcessor("timestamp-url", (source, el, ctx) => {
-			const url = source.trim();
-			if (ReactPlayer.canPlay(url)) {
-				// Creates button for video url
-				const div = el.createEl("div");
-				const button = div.createEl("button");
-				button.innerText = url;
-				button.style.backgroundColor = this.settings.urlColor;
-				button.style.color = this.settings.urlTextColor;
+		this.registerMarkdownCodeBlockProcessor(
+			"timestamp-url",
+			(source, el, ctx) => {
+				const url = source.trim();
+				if (ReactPlayer.canPlay(url)) {
+					// Creates button for video url
+					const div = el.createEl("div");
+					const button = div.createEl("button");
+					button.innerText = url;
+					button.style.backgroundColor = this.settings.urlColor;
+					button.style.color = this.settings.urlTextColor;
 
-				button.addEventListener("click", () => {
-					this.activateView(url, this.editor);
-				});
-			} else {
-				if (this.editor) {
-					this.editor.replaceSelection(this.editor.getSelection() + "\n" + ERRORS["INVALID_URL"]);
+					button.addEventListener("click", () => {
+						this.activateView(url, this.editor);
+					});
+				} else {
+					if (this.editor) {
+						this.editor.replaceSelection(
+							this.editor.getSelection() +
+								"\n" +
+								ERRORS["INVALID_URL"]
+						);
+					}
 				}
 			}
-		});
+		);
 
 		// Command that gets selected video link and sends it to view which passes it to React component
 		this.addCommand({
-			id: 'trigger-player',
-			name: 'Open video player (copy video url and use hotkey)',
+			id: "trigger-player",
+			name: "Open video player (copy video url and use hotkey)",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				// Get selected text and match against video url to convert link to video video id
-				const url = editor.getSelection().trim();
+
+				// can this use getLinkpath()? ; is there parsed markdown avaliable?
+				const url = editor
+					.getSelection()
+					.trim() // remove any leading or trailing whitespace, then
+					.replace(/^!?\[{1,2}/, "") // remove any (internal or external) link-syntax front-matter
+					.replace(/^[^\]]*\]\(/, "") //if it was an external link, gobble the text, if any, and the open-paren
+					.replace(/\)$|\]$|\]]$/, ""); // and any closing paren, closing square-brace(s)
 
 				// Activate the view with the valid link
 				if (ReactPlayer.canPlay(url)) {
 					this.activateView(url, editor);
-					this.settings.noteTitle ?
-						editor.replaceSelection("\n" + this.settings.noteTitle + "\n" + "```timestamp-url \n " + url + "\n ```\n") :
-						editor.replaceSelection("```timestamp-url \n " + url + "\n ```\n")
+					this.settings.noteTitle
+						? editor.replaceSelection(
+								"\n" +
+									this.settings.noteTitle +
+									"\n" +
+									"```timestamp-url \n " +
+									url +
+									"\n ```\n"
+						  )
+						: editor.replaceSelection(
+								"```timestamp-url \n " + url + "\n ```\n"
+						  );
 					this.editor = editor;
 				} else {
-					editor.replaceSelection(ERRORS["INVALID_URL"])
+					editor.replaceSelection(ERRORS["INVALID_URL"]);
 				}
-				editor.setCursor(editor.getCursor().line + 1)
-			}
+				editor.setCursor(editor.getCursor().line + 1);
+			},
 		});
 
 		// This command inserts the timestamp of the playing video into the editor
 		this.addCommand({
-			id: 'timestamp-insert',
-			name: 'Insert timestamp based on videos current play time',
+			id: "timestamp-insert",
+			name: "Insert timestamp based on videos current play time",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				if (!this.player) {
-					editor.replaceSelection(ERRORS["NO_ACTIVE_VIDEO"])
-					return
+					editor.replaceSelection(ERRORS["NO_ACTIVE_VIDEO"]);
+					return;
 				}
 
 				// convert current video time into timestamp
-				const leadingZero = (num: number) => num < 10 ? "0" + num.toFixed(0) : num.toFixed(0);
-				const totalSeconds = Number(this.player.getCurrentTime().toFixed(2));
+				const leadingZero = (num: number) =>
+					num < 10 ? "0" + num.toFixed(0) : num.toFixed(0);
+				const totalSeconds = Number(
+					this.player.getCurrentTime().toFixed(2)
+				);
 				const hours = Math.floor(totalSeconds / 3600);
-				const minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
-				const seconds = totalSeconds - (hours * 3600) - (minutes * 60);
-				const time = (hours > 0 ? leadingZero(hours) + ":" : "") + leadingZero(minutes) + ":" + leadingZero(seconds);
+				const minutes = Math.floor((totalSeconds - hours * 3600) / 60);
+				const seconds = totalSeconds - hours * 3600 - minutes * 60;
+				const time =
+					(hours > 0 ? leadingZero(hours) + ":" : "") +
+					leadingZero(minutes) +
+					":" +
+					leadingZero(seconds);
 
 				// insert timestamp into editor
-				editor.replaceSelection("```timestamp \n " + time + "\n ```\n")
-			}
+				editor.replaceSelection("```timestamp \n " + time + "\n ```\n");
+			},
 		});
 
 		//Command that play/pauses the video
 		this.addCommand({
-			id: 'pause-player',
-			name: 'Pause player',
+			id: "pause-player",
+			name: "Pause player",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				this.setPlaying(!this.player.props.playing)
-			}
+				this.setPlaying(!this.player.props.playing);
+			},
 		});
 
 		// Seek forward by set amount of seconds
 		this.addCommand({
-			id: 'seek-forward',
-			name: 'Seek Forward',
+			id: "seek-forward",
+			name: "Seek Forward",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				if (this.player) this.player.seekTo(this.player.getCurrentTime() + parseInt(this.settings.forwardSeek));
-			}
+				if (this.player)
+					this.player.seekTo(
+						this.player.getCurrentTime() +
+							parseInt(this.settings.forwardSeek)
+					);
+			},
 		});
 
 		// Seek backwards by set amount of seconds
 		this.addCommand({
-			id: 'seek-backward',
-			name: 'Seek Backward',
+			id: "seek-backward",
+			name: "Seek Backward",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				if (this.player) this.player.seekTo(this.player.getCurrentTime() - parseInt(this.settings.backwardsSeek));
-			}
+				if (this.player)
+					this.player.seekTo(
+						this.player.getCurrentTime() -
+							parseInt(this.settings.backwardsSeek)
+					);
+			},
 		});
 
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: "open-local-video",
+			name: "Open Local Video",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				this.editor = editor;
-				new SampleModal(this.app, this.activateView.bind(this), editor).open();
+				new LocalVideoModal(
+					this.app,
+					this.activateView.bind(this),
+					editor
+				).open();
 				// This command will only show up in Command Palette when the check function returns true
 				return true;
-			}
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
@@ -171,8 +226,14 @@ export default class TimestampPlugin extends Plugin {
 	}
 
 	// This is called when a valid url is found => it activates the View which loads the React view
-	async activateView(url: string, editor: Editor) {
+	async activateView(url: string | File, editor: Editor): Promise<void> {
 		this.app.workspace.detachLeavesOfType(VIDEO_VIEW);
+
+		if (url instanceof File) {
+			return await this.activateView(_urlFromFile(url), editor);
+		} else if (!url.contains("://")) {
+			return await this.activateView(await _urlFromFilePath(url), editor);
+		}
 
 		await this.app.workspace.getRightLeaf(false).setViewState({
 			type: VIDEO_VIEW,
@@ -186,22 +247,30 @@ export default class TimestampPlugin extends Plugin {
 		// This triggers the React component to be loaded
 		this.app.workspace.getLeavesOfType(VIDEO_VIEW).forEach(async (leaf) => {
 			if (leaf.view instanceof VideoView) {
-
-				const setupPlayer = (player: ReactPlayer, setPlaying: React.Dispatch<React.SetStateAction<boolean>>) => {
+				const setupPlayer = (
+					player: ReactPlayer,
+					setPlaying: React.Dispatch<React.SetStateAction<boolean>>
+				) => {
 					this.player = player;
 					this.setPlaying = setPlaying;
-				}
+				};
 
 				const setupError = (err: string) => {
-					editor.replaceSelection(editor.getSelection() + `\n> [!error] Streaming Error \n> ${err}\n`);
-				}
+					editor.replaceSelection(
+						editor.getSelection() +
+							`\n> [!error] Streaming Error \n> ${err}\n`
+					);
+				};
 
 				const saveTimeOnUnload = async () => {
 					if (this.player) {
-						this.settings.urlStartTimeMap.set(url, Number(this.player.getCurrentTime().toFixed(0)));
+						this.settings.urlStartTimeMap.set(
+							url,
+							Number(this.player.getCurrentTime().toFixed(0))
+						);
 					}
 					await this.saveSettings();
-				}
+				};
 
 				// create a new video instance, sets up state/unload functionality, and passes in a start time if available else 0
 				leaf.setEphemeralState({
@@ -209,7 +278,7 @@ export default class TimestampPlugin extends Plugin {
 					setupPlayer,
 					setupError,
 					saveTimeOnUnload,
-					start: ~~this.settings.urlStartTimeMap.get(url)
+					start: ~~this.settings.urlStartTimeMap.get(url),
 				});
 
 				await this.saveSettings();
@@ -219,12 +288,25 @@ export default class TimestampPlugin extends Plugin {
 
 	async loadSettings() {
 		// Fix for a weird bug that turns default map into a normal object when loaded
-		const data = await this.loadData()
+		const data = await this.loadData();
 		if (data) {
-			const map = new Map(Object.keys(data.urlStartTimeMap).map(k => [k, data.urlStartTimeMap[k]]))
-			this.settings = { ...DEFAULT_SETTINGS, ...data, urlStartTimeMap: map };
+			const map = new Map(
+				Object.keys(data.urlStartTimeMap).map((k) => [
+					k,
+					data.urlStartTimeMap[k],
+				])
+			);
+			this.settings = {
+				...DEFAULT_SETTINGS,
+				...data,
+				urlStartTimeMap: map,
+			};
 		} else {
-			this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+			this.settings = Object.assign(
+				{},
+				DEFAULT_SETTINGS,
+				await this.loadData()
+			);
 		}
 	}
 
@@ -233,10 +315,14 @@ export default class TimestampPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
+class LocalVideoModal extends Modal {
 	editor: Editor;
 	activateView: (url: string, editor: Editor) => void;
-	constructor(app: App, activateView: (url: string, editor: Editor) => void, editor: Editor) {
+	constructor(
+		app: App,
+		activateView: (url: string, editor: Editor) => void,
+		editor: Editor
+	) {
 		super(app);
 		this.activateView = activateView;
 		this.editor = editor;
@@ -246,21 +332,65 @@ class SampleModal extends Modal {
 		const { contentEl } = this;
 		// add an input field to contentEl
 
-		const input = contentEl.createEl('input');
+		const input = contentEl.createEl("input");
 		input.setAttribute("type", "file");
 		input.onchange = (e: any) => {
-			// accept local video input and make a url from input
-			const url = URL.createObjectURL(e.target.files[0]);
-			this.activateView(url, this.editor);
+			const file = e.target.files[0];
+			this.activateView(file, this.editor);
 
-			// Can't get the buttons to work with local videos unfortunately
-			// this.editor.replaceSelection("\n" + "```timestamp-url \n " + url.trim() + "\n ```\n")
+			this.editor.replaceSelection(
+				"\n" + "```timestamp-url \n " + file.path + "\n ```\n"
+			);
 			this.close();
-		}
+		};
 	}
 
 	onClose() {
 		const { contentEl } = this;
 		contentEl.empty();
 	}
+}
+
+function _urlFromFile(file: File) {
+	if (file instanceof File) {
+		return URL.createObjectURL(file);
+	} else {
+		return _videoNotFoundUrl;
+	}
+}
+
+async function _urlFromFilePath(fpath: string): Promise<string> {
+	const path = require("path");
+	const normalizedFPath = path.normalize(fpath);
+
+	if (path.isAbsolute(normalizedFPath)) {
+		if (path.isAbsolute(normalizedFPath)) {
+			return _urlFromFile(
+				//XXX: being this direct will fail on files >2GiB and I'm not sure how to get it to behave
+				new File(
+					[await app.vault.adapter.readBinary(normalizedFPath)],
+					path.basename(normalizedFPath)
+				)
+			);
+		}
+	}
+
+	let vaultFile: TFile = null;
+	if (vaultFile === null) {
+		for (let f of this.app.vault.getFiles()) {
+			if (f.path.endsWith(fpath)) {
+				vaultFile = f;
+				break;
+			}
+		}
+		if (vaultFile === null) {
+			return _videoNotFoundUrl;
+		}
+	}
+
+	if (!app.metadataCache.getFileCache(vaultFile)) {
+		app.vault.readBinary(vaultFile); // kick off a read, but we don't need to await it… probably
+	}
+
+	return app.vault.getResourcePath(vaultFile);
 }
